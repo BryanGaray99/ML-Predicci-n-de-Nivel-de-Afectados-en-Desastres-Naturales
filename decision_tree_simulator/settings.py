01370d55707a5cf6f9e7c6addd90c5a592d95c02
Bryan Garay <bryangarayacademico@gmail.com>
2024-06-28 14:15:04 -0500

    Update ALLOWED_HOSTS for Render deployment

--- decision_tree_simulator/settings.py
+++ decision_tree_simulator/settings.py
@@ -38,12 +38,12 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', '').split(',')]
 
 
 # Application definition
